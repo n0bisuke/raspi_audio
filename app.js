f2c6a27f8be9e7472f96ee62a312d60d8a963db9
のびすけ <rsksound@gmail.com>
2018-05-16 21:07:18 +0900

    update

--- app.js
+++ app.js
@@ -2,17 +2,20 @@
 
 const player = require('./libs/playsound');
 const ytdl = require('./libs/ytdl');
-const outputfile = './audiofile/output.mp4';
-const outputfile2 = './audiofile/output2.mp4';
-let playList = ['IPSyweVhyTE','90AiXO1pAiA','zVQ7qd2ukn8'];
 
-const init = async () => {
+let playList = ['zVQ7qd2ukn8','90AiXO1pAiA','IPSyweVhyTE'];
+let count = 1;
+let outputfile = './audiofile/output.mp4';
+
+const main = async () => {
+    console.log(`${count}曲目~`);
     let res = '';
     const playVideoId = playList.shift();
     res = await ytdl(playVideoId,outputfile);
     console.log(res);
-    console.log(`-----${playList.toString()}-----`);
+    console.log(`-----playlist=${playList.toString()}-----`);
     res = await player(outputfile);
     console.log(res);
+    main();
 };
-init();
+main();
